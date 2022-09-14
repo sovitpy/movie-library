228040cdc4a0d09204aa6e604718401026360d62
sovitpy <sovitlekhak@gmail.com>
2022-09-14 00:17:43 -0500

    feat: added search feature

--- src/components/Movielist.jsx
+++ src/components/Movielist.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
-
 const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, idx) => (
-        <div className="d-flex justify-content-start m-3">
-          <img src={movie.Poster} alt={movie.Title} key={idx} />
+        <div className="d-flex justify-content-start m-3" key={idx}>
+          <img src={movie.Poster} alt={movie.Title} />
         </div>
       ))}
     </>
